a5417fb33ee00296ef1474a6519d69e9d7104834
Jason_Smith <qili00001@gmail.com>
2019-11-28 17:03:56 +0800

    Create IP_StrToHex.c

---
+++ IP_StrToHex.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+
+/*******************************************************************************
+  * @brief  将IP改为十六进制
+  * @param  (ip字符串类型，ip十六进制类型，ip端口)
+            前一个为输入，后两个为输出
+  * @retval None
+*******************************************************************************/
+void IP_StrToHex(unsigned char *_ipstr, unsigned char *_iphex, unsigned int *_port)
+{
+//  uint16_t i = 0;
+  unsigned char ip_buf[22];
+  unsigned char *p1 = ip_buf;
+  unsigned char *p2 = ip_buf;
+  unsigned char* addr = (unsigned char*) _iphex;
+  unsigned int* port = _port;
+  
+//  memcpy(ip_buf,_str,strlen(_str));
+  strcpy(ip_buf, (&_ipstr[0]) );
+  
+  while(*p1++ != '.');
+  *addr = atoi(p2);
+  
+  p2 = p1;
+  *(p1-1) = 0;
+  *(addr+1) = atoi(p2);
+  
+  while(*p1++ != '.');
+  p2 = p1;
+  *(p1-1) = 0;
+  *(addr+2) = atoi(p2);
+  
+  while(*p1++ != '.');
+  p2 = p1;
+  *(p1-1) = 0;
+  *(addr+3) = atoi(p2);
+  
+  while(*p1++ != ',');
+  p2 = p1;
+  *(p1-1) = 0;
+  *port = atoi(p2);
+
+}
