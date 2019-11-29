08b56ee4cb7e5ef4de06372dd8765b023c2b3061
Jason_Smith <qili00001@gmail.com>
2019-11-29 09:40:50 +0800

    Create CaculateWeekDay.c
    1. 添加计算某天为周几的算法

---
+++ CaculateWeekDay.c
@@ -0,0 +1,17 @@
+/*******************************************************************************
+  * @brief  基姆拉尔森计算公式
+  * @param  
+  * @retval None
+*******************************************************************************/
+static char CaculateWeekDay(int yyyy,char mm, char dd)
+{
+  if( mm==1 || mm==2)
+  {
+    mm+=12;
+    yyyy--;
+  }
+  char iWeek;
+  iWeek = (dd + 2*mm + 3*(mm+1)/5+yyyy + yyyy/4 - yyyy/100 + yyyy/400) % 7;
+
+  return (iWeek+1);
+}
